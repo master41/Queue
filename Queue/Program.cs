b00e1dd2f0c3d32d3a9b8537961632b22243ef67
master41 <sharnin1996@gmail.com>
2022-04-09 22:28:26 +0300

    Добавьте файлы проекта.

---
+++ Queue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("Кафедра");
+            queue.Enqueue("комп'ютерних");
+            queue.Enqueue("інформаційних");
+            queue.Enqueue("технологій");
+
+            while (!queue.IsEmpty)
+            { Console.Write(queue.Dequeue() + " "); }
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Queue/Queue.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    internal class Queue<T>:IEnumerable<T>
+    {
+        // Посидання на найстаріший вузол.
+        private Node<T> first;
+        // Посидання на найсвіжіший вузол.
+        private Node<T> last;
+        // Кількість елементів у черзі.
+        private int count;
+
+        public bool IsEmpty { get => first == null; }
+        public int Count { get => count; }
+
+        // Додавання нового елемента у кінець зв'язного списку.
+        public void Enqueue(T component)
+        {
+            // Збереження посилання на кінцевий вузол.
+            Node<T> oldlast = last;
+            // Створення нового кінцевого вузла.
+            last = new Node<T>();
+            last.item = component;
+            last.next = null;
+            // Початок і кінець списку вказують на єдиний елемент.
+            if (IsEmpty)
+            { first = last; }
+            else
+            // Прив'язка нового вузла до кінця списку.
+            { oldlast.next = last; }
+            count++;
+        }
+
+        public T Dequeue()
+        {
+            // Видалення елементу із початку списку.
+            T component = first.item;
+            first = first.next;
+            if (IsEmpty)
+            { last = null; }
+            count--;
+            return component;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> current = first;
+            while (current != null)
+            {
+                yield return current.item;
+                current = current.next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
